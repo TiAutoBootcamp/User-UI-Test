9e4a452acf5e1aa05068c953e5f6c9e3033f2b38
Nahuel Rodriguez <nahuel.rodriguez@teaminternational.com>
2023-08-18 13:40:44 -0300

    transtaction tab loader autotests

---
+++ UserUITest/Utils/PageFactoryUtil.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagementServiceUITests.Utils
+{
+    public class PageFactoryUtil
+    {
+        [FindsBy(How = How.CssSelector, Using = ".mud-progress-circular-svg")]
+        private IWebElement progressElement;
+
+        public void WaitForProgressToDisappear(WebDriverWait wait)
+        {
+            wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.CssSelector(".mud-progress-circular-svg")));
+        }
+    }
+}
